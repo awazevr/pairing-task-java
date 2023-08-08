7ae40d1173d84ed6bd28f3e1bad7c0ca4dbf83d2
Will Thompson <Will.360@outlook.com>
2023-08-08 13:55:32 +0100

    Updated to latest Java LTS version (#1)
    * Updated to latest Java LTS version
    
    * Updated to junit 5 from 4
    
    ---------
    
    Co-authored-by: Will Thompson <willthompson@awaze.com>

--- src/test/java/area/AreaFinderTest.java
+++ src/test/java/area/AreaFinderTest.java
@@ -1,58 +1,58 @@
 package area;
 
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AreaFinderTest {
-	private static final String DATASET_CSV = "dataset.csv";
-	private static final String EXPECTED_DATASET_CSV = "expected_dataset.csv";
+    private static final String DATASET_CSV = "dataset.csv";
+    private static final String EXPECTED_DATASET_CSV = "expected_dataset.csv";
 
-	private static List<DataPoint> dataset;
-	private static List<DataPoint> expectedResult;
-	private static Rectangle rect;
+    private static List<DataPoint> dataset;
+    private static List<DataPoint> expectedResult;
+    private static Rectangle rect;
 
-	@BeforeClass
-	public static void loadData() throws Exception{
-		dataset = DataSet.readDataPoints(DATASET_CSV);
-		expectedResult = DataSet.readDataPoints(EXPECTED_DATASET_CSV);
-		rect = new Rectangle(-0.1f, -0.1f, 0.1f, 0.1f);
-	}
+    @BeforeAll
+    public static void loadData() throws Exception {
+        dataset = DataSet.readDataPoints(DATASET_CSV);
+        expectedResult = DataSet.readDataPoints(EXPECTED_DATASET_CSV);
+        rect = new Rectangle(-0.1f, -0.1f, 0.1f, 0.1f);
+    }
 
-	@Test
-	public void testBruteForceAreaFinder() throws Exception {
-		loadDataset(BruteForceAreaFinder.constructFrom(dataset));
-	}
+    @Test
+    public void testBruteForceAreaFinder() throws Exception {
+        loadDataset(BruteForceAreaFinder.constructFrom(dataset));
+    }
 
-	private void loadDataset(AreaFinder finder){
-		PointCollector collector = new PointCollector();
-		finder.findPointsInside(rect, collector);
-		List<DataPoint> actualResult = collector.getConsumedPoints();
-		assertTrue(areIdentical(actualResult, expectedResult));
-	}
+    private void loadDataset(AreaFinder finder) {
+        PointCollector collector = new PointCollector();
+        finder.findPointsInside(rect, collector);
+        List<DataPoint> actualResult = collector.getConsumedPoints();
+        assertTrue(areIdentical(actualResult, expectedResult));
+    }
 
-	private boolean areIdentical(List<DataPoint> actualResults, List<DataPoint> expectedResults) {
-		return   actualResults.size() == expectedResults.size()
-				&& actualResults.containsAll(expectedResults)
-				&& expectedResults.containsAll(actualResults);
-	}
+    private boolean areIdentical(List<DataPoint> actualResults, List<DataPoint> expectedResults) {
+        return actualResults.size() == expectedResults.size()
+                && actualResults.containsAll(expectedResults)
+                && expectedResults.containsAll(actualResults);
+    }
 
-	private class PointCollector implements PointConsumer {
+    private class PointCollector implements PointConsumer {
 
-		List <DataPoint> results = new ArrayList<DataPoint>();
+        List<DataPoint> results = new ArrayList<DataPoint>();
 
-		@Override
-		public void consumePoint(DataPoint point) {
-			//todo : implement
-		}
+        @Override
+        public void consumePoint(DataPoint point) {
+            //todo : implement
+        }
 
-		public List<DataPoint> getConsumedPoints(){
-			return  results;
-		}
-	}
+        public List<DataPoint> getConsumedPoints() {
+            return results;
+        }
+    }
 
 }
